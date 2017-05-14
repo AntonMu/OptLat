49e5a0d2274143596a25af1e0bd32ca08fc72be7
AntonMu <muehlemann@maths.ox.ac.uk>
2017-05-13 18:29:57 -0700

    Revert "Add files via upload"
    This reverts commit 07e908f988c0bba20873357e61f73f45108a3f46.

--- Source/optdef.m
+++
@@ -1,6 +0,0 @@
-function s= optdef(Q,Fi,G)
-x=[];
-for i=1:size(Q)
-    x(i,:)=reshape(pdec(G*reshape(Q(i,:),[3,3])*Fi),[1,9]);
-end
-s = unique(round(x,8), 'rows');
--- Source/volchange.m
+++
@@ -1,3 +0,0 @@
-function z=volchange(x)
-z=round(100*max(1/x,x));
-end
